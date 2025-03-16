389aa782eaad5aa2441994a7a45d72742bb36b56
khmiq <mumtozabonukhamidullayeva7@gmail.com>
2025-03-16 09:13:59 +0500

    fixed cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://your-frontend-domain.vercel.app"],
+    origin: ["http://localhost:5173", "https://my-word-frontend.vercel.app/"],
     methods: ["GET", "POST", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type"],
 };
