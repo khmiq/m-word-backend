b7faead334a87bf0bffb02180654b506a7a36db5
khmiq <mumtozabonukhamidullayeva7@gmail.com>
2025-03-16 09:10:34 +0500

    Fixed CORS issue

--- server.js
+++ server.js
@@ -7,8 +7,13 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({origin: 'https://my-word-bbu070haj-khmiqs-projects.vercel.app/'}));
+const corsOptions = {
+    origin: ["http://localhost:5173", "https://your-frontend-domain.vercel.app"],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+};
 
+app.use(cors(corsOptions));
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -21,3 +26,5 @@ app.use("/words", wordRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
